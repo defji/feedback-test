624c104df10cbf5e0d921e698f4133dce6a8b93b
dfj <balazs.gyorkos@gmail.com>
2016-05-03 15:31:54 +0200

    eleje

---
+++ resources/views/feedback.blade.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <title>Crosssec feedback</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+</head>
+<body>
+<article>
+    <div class="container" style="padding-top: 5em">
+        <div class="panel panel-primary">
+            <div class="panel-heading">Panel heading without title</div>
+            <div class="panel-body">
+                <form>
+                    <div class="col-md-6">
+                        <label for="username" class="control-label">Felhasználónév</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="email" class="control-label">E-mail cím</label>
+                        <input type="email" class="form-control">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="password" class="control-label">Jelszó</label>
+                        <input type="password" class="form-control">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="password" class="control-label">Jelszó ismét</label>
+                        <input type="password" class="form-control">
+                    </div>
+                    <div class="col-md-6">
+                        <label>Felhasználás jellege</label>
+                        <div class="radio">
+                            <label><input type="radio" name="optradio">Option 1</label>
+                        </div>
+                        <div class="radio">
+                            <label><input type="radio" name="optradio">Option 2</label>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
+</article>
+</body>
+</html>
